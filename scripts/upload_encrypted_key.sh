e68a32f93cef846f35936e25905884a728aef2f4
Faizal Zakaria <phaibusiness@gmail.com>
2015-11-29 13:15:05 +0800

    Options for interactive for upload encrypted key script

--- scripts/upload_encrypted_key.sh
+++ scripts/upload_encrypted_key.sh
@@ -1,5 +1,43 @@
 #!/bin/sh
 
-for server in `knife node list` ; do
-  scp -o StrictHostKeyChecking=no ~/.chef/encrypted_data_bag_secret root@$server:/tmp/; ssh -o StrictHostKeyChecking=no root@$server 'sudo mv /tmp/encrypted_data_bag_secret /etc/chef/';
+
+SCRIPTS_DIR=$(dirname ${0:-})
+ROOT_DIR="$SCRIPTS_DIR/.."
+
+source $SCRIPTS_DIR/utils.sh
+
+UPLOAD_FROM_CHEF_SERVER=true
+
+usage() { echo "Usage: $0 [-i (for interactive)]" 1>&2; exit 1; }
+
+while getopts "i" opt; do
+  case $opt in
+    i)
+      UPLOAD_FROM_CHEF_SERVER=false
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
+
+
+#
+# $1: server
+#
+function upload_keys
+{
+  local _server=$1
+  scp -o StrictHostKeyChecking=no ~/.chef/encrypted_data_bag_secret root@$_server:/tmp/; ssh -o StrictHostKeyChecking=no root@$_server 'sudo mv /tmp/encrypted_data_bag_secret /etc/chef/';
+}
+
+if [ "$UPLOAD_FROM_CHEF_SERVER" == "true" ]; then
+  for server in `knife node list` ; do
+    upload_keys $server
+  done
+else
+  p_info "Whats the server IP?"
+  read server
+  upload_keys $server
+fi
+
